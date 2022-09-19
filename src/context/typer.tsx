8adc8463c00b5ea9becb3d79dfcbc77c163dd279
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-09-20 00:28:29 +0530

    feat: sent user typing data

--- src/context/typer.tsx
+++ src/context/typer.tsx
@@ -1,5 +1,6 @@
 import React, { useState, createContext, useEffect, useContext } from 'react';
 import { faker } from '@faker-js/faker';
+import { useClient } from 'react-supabase';
 
 const TyperContext = createContext<any>({});
 
@@ -12,6 +13,19 @@ export const TyperProvider = ({ children }: { children: React.ReactNode }) => {
     const [grossWords, setGrossWords] = useState<number>(0);
     const [started, setStarted] = useState<boolean>(false);
     const [sentence, setSentence] = useState<string[]>([]);
+    const client = useClient();
+
+    const sentServer = async () => {
+        const { error } = await client.from('typing_data').insert([
+            {
+                user_id: client.auth.user().id,
+                wpm,
+                accuracy,
+            },
+        ]);
+        // eslint-disable-next-line @typescript-eslint/no-throw-literal
+        if (error) throw error;
+    };
     useEffect(() => {
         setSentence(faker.random.words(40).split(' '));
     }, []);
@@ -31,11 +45,13 @@ export const TyperProvider = ({ children }: { children: React.ReactNode }) => {
                 }
                 if (time === 0) {
                     clearInterval(interval);
+                    sentServer();
                 }
             }, 1000);
             return () => clearInterval(interval);
         }
         return () => {};
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [started, time]);
 
     return (
